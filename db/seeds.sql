73703e491e64152f7f26064ce215db85092eee8d
Evelyn Coleman <coleve27@gmail.com>
2018-02-12 20:07:10 -0600

    worked on app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Monteray Jack', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Texas', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Mushroom Swiss', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Buffalo Chicken', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Veggie', true);
